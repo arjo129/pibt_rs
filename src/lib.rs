38e1df1d6b977ffe14f7d422f40768350d4e41f3
Arjo Chakravarty <arjoc@google.com>
2025-09-16 08:52:42 +0800

    Added collision checker
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

--- src/lib.rs
+++ src/lib.rs
@@ -249,17 +249,87 @@ pub fn parse_scen(file_name: &str) -> Result<Vec<Vec<(usize, usize)>>, std::num:
     Ok(vec![starts, ends])
 }
 
-struct CollisionChecker {
-    grid_sizes: Vec<usize>,
+/// When converting between coordinate space, the grid clip mode
+/// is in charge of figuring out how to handle rounding.
+enum GridClipMode {
+    CONSERVATIVE_TOP_LEFT,
+    CONSERVATIVE_BOTTOM_RIGHT,
 }
 
-impl CollisionChecker {
+/// Internal collision checking structure for mapping between grid cells
+struct MultiGridCollisionChecker {
+    /// The size of an individual grid.
+    /// It is assumed that each grid starts at the same top left corner
+    grid_sizes: Vec<f32>,
+}
+
+impl MultiGridCollisionChecker {
     /// Gets the other blocked node
     fn get_blocked_nodes(&self, grid_id: usize, x: usize, y: usize) -> Vec<(usize, usize, usize)> {
-        vec![]
+        // Get the absolute x,y position in grid space
+        let target_size = self.grid_sizes[grid_id];
+        let (real_world_x, real_world_y) = (x as f32 * target_size, y as f32 * target_size);
+
+        // Get the grid sizes
+        let mut grids = vec![];
+        for (id, &grid_size) in self.grid_sizes.iter().enumerate() {
+            if id == grid_id {
+                continue;
+            }
+            let (tl_x, tl_y) = (real_world_x, real_world_y);
+            let (br_x, br_y) = (real_world_x + target_size, real_world_y + target_size);
+            let (start_x, start_y) =
+                self.get_grid_space(id, tl_x, tl_y, GridClipMode::CONSERVATIVE_TOP_LEFT);
+
+            let (end_x, end_y) =
+                self.get_grid_space(id, br_x, br_y, GridClipMode::CONSERVATIVE_BOTTOM_RIGHT);
+
+            for dx in start_x..end_x {
+                for dy in start_y..end_y {
+                    grids.push((id, dx, dy));
+                }
+            }
+        }
+        return grids;
+    }
+
+    fn get_grid_space(&self, grid_id: usize, x: f32, y: f32, mode: GridClipMode) -> (usize, usize) {
+        let coords = (
+            (x / self.grid_sizes[grid_id]),
+            (y / self.grid_sizes[grid_id]),
+        );
+
+        match (mode) {
+            GridClipMode::CONSERVATIVE_TOP_LEFT => {
+                (coords.0.floor() as usize, coords.1.floor() as usize)
+            }
+            GridClipMode::CONSERVATIVE_BOTTOM_RIGHT => {
+                (coords.0.ceil() as usize, coords.1.ceil() as usize)
+            }
+        }
     }
 }
 
+#[cfg(test)]
+#[test]
+fn test_grid_space() {
+    let mut shared_grid_space = MultiGridCollisionChecker {
+        grid_sizes: vec![1.0, 2.0],
+    };
+    let other_blocked_nodes = shared_grid_space.get_blocked_nodes(0, 1, 1);
+    let other_blocked_nodes2 = shared_grid_space.get_blocked_nodes(1, 0, 0);
+
+    println!("{:?}", other_blocked_nodes);
+    println!("{:?}", other_blocked_nodes2);
+    assert!(other_blocked_nodes.len() == 1);
+    assert!(other_blocked_nodes.contains(&(1, 0, 0)));
+
+    assert!(other_blocked_nodes2.len() == 4);
+    assert!(other_blocked_nodes2.contains(&(0, 0, 1)));
+    assert!(other_blocked_nodes2.contains(&(0, 1, 0)));
+    assert!(other_blocked_nodes2.contains(&(0, 1, 1)));
+    assert!(other_blocked_nodes2.contains(&(0, 0, 0)));
+}
 struct CBSPiBT {}
 
 impl CBSPiBT {}
